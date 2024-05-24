938dc2cdc7b54228fdef4372bb0b1dcafe244853
thewb <thewb@users.noreply.github.com>
2024-05-24 08:34:17 -0700

    ch ch ch changes

--- pass-gen/ContentView.swift
+++ pass-gen/ContentView.swift
@@ -1,37 +1,55 @@
-//
-//  ContentView.swift
-//  pass-gen
-//
-//  Created by jb on 5/13/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedLength: Int? = nil
+    let lengths = [32, 16, 12, 8]
+
     var body: some View {
         VStack {
             Button(action: {
-                ClipboardManager.doit()
+                ClipboardManager.doit(length: selectedLength ?? 32)
             }) {
                 Text("new")
-            }   .cornerRadius(8)
-                .background(Color.black).opacity(0.8)
-                                
+            }
+            .cornerRadius(8)
+            .background(Color.black.opacity(0.8))
+
             Button(action: {
                 ClipboardManager._exit()
             }) {
                 Text("exit")
-            }   .cornerRadius(8)
-                .background(Color.black).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-            
+            }
+            .cornerRadius(8)
+            .background(Color.black.opacity(0.8))
+
             Button(action: {
                 ClipboardManager._clear()
             }) {
                 Text("clear")
-            }   .cornerRadius(8)
-                .background(Color.black).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+            }
+            .cornerRadius(8)
+            .background(Color.black.opacity(0.8))
+
+            Menu("length") {
+                ForEach(lengths, id: \.self) { length in
+                    Button(action: {
+                        self.selectedLength = length
+                    }) {
+                        HStack {
+                            Text("\(length)")
+                            if selectedLength == length {
+                                Spacer()
+                                Image(systemName: "checkmark")
+                            }
+                        }
+                    }
+                }
+                
+            }
+            .frame(width: 80)
+            .cornerRadius(8)
+            .background(Color.black.opacity(0.8))
+            .foregroundColor(.white)
         }
     }
 }
-
-
--- pass-gen/actions.swift
+++ pass-gen/actions.swift
@@ -15,15 +15,15 @@ class ClipboardManager {
         pasteboard.setString(text, forType: .string)
     }
     
-    static func generate() -> String {
+    static func generate(length: Int) -> String {
         let charset: [Character] = (32...126).compactMap{Character(UnicodeScalar($0))}
-        let passw: [Character] = ((0..<32).map{_ in charset.randomElement()!})
+        let passw: [Character] = ((0..<length).map{_ in charset.randomElement()!})
         let rv = String(passw)
         return rv
     }
         
-    static func doit() {
-        let pword = generate()
+    static func doit(length: Int) {
+        let pword = generate(length: length)
         copyToClipboard(text: pword)
     }
     
